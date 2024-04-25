9cbfa7745726fe04acdcc3bdbae33fe08c590529
karan mandhare <2002karanmandhare@gmail.com>
2024-04-25 21:31:14 +0530

    connection with mongodb

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-console.log('hello');
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+connectDB();
+
+/*
+import express from "express";
+const app = express()(async () => {
+  try {
+    const connect = await mongoose.connect(
+      `${process.env.MONGODB_URL}/${DB_NAME}`
+    );
+
+    app.on("error", (err) => {
+      console.log("ERROR: ", err);
+      throw err;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on port: ${process.env.PORT}`);
+    });
+  } catch (err) {
+    console.log("ERROR: ", err);
+  }
+})();
+*/
